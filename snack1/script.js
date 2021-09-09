a96a123bb33157039f5a5d2f9c20387192bd7e5d
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-09 14:28:28 +0200

    creata struttura

---
+++ snack1/script.js
@@ -0,0 +1,3 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
