4a99b33e502198172294b243bda4c68654eae782
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-09 15:04:47 +0200

    snack1

--- snack1/script.js
+++ snack1/script.js
@@ -1,3 +1,31 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const biciCorsa=[
+    {
+    "nome":"rossa",
+    "peso":9
+},
+{
+    "nome":"verde",
+    "peso":12
+},
+{
+    "nome":"bianchi",
+    "peso":17
+},
+{
+    "nome":"blu",
+    "peso":15
+}
+
+];
+let biciLeggera=biciCorsa[0];
+for(let i=0;i<biciCorsa.length;i++){
+    if(biciCorsa[i].peso<biciLeggera.peso){
+        biciLeggera=biciCorsa[i];
+    };
+}
+const{nome,peso}=biciLeggera;
+console.log(`la bici piu leggera è ${nome} e pesa ${peso}kg`);
