93b5b61e70f05214111c131e7f7689ed9b7f51d3
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-09 16:31:32 +0200

    creata struttura e arrey snack2

--- snack2/script.js
+++ snack2/script.js
@@ -3,3 +3,38 @@
 // Generare numeri random al posto degli 0 nelle proprietà:
 // Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre=[
+    {
+        "nome":"inter",
+        "puntiFatti":0,
+        "falliSubiti":0
+    },
+    {
+        "nome":"milan",
+        "puntiFatti":0,
+        "falliSubiti":0
+    },
+    {
+        "nome":"juve",
+        "puntiFatti":0,
+        "falliSubiti":0
+    },
+    {
+        "nome":"roma",
+        "puntiFatti":0,
+        "falliSubiti":0
+    },
+];
+
+const random=(min,max)=>Math.floor(Math.random()*(max-min+1))+min;
+let puntiFatti=random(1,20);
+let falliSubiti=random(1,40);
+
+
+for(var i=0;i<squadre;i++){
+     squadre[i].push(puntiFatti);
+     squadre[i].push(falliSubiti);
+} 
+ console.log(squadre);
+ 
